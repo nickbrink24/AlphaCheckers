48b26013f77c019966c6776ce44ac2a1aa58725f
Ethan <ethan.e.nguyen@outlook.com>
2023-03-28 09:26:02 -0700

    test

--- app/src/main/java/com/example/gamestate/CheckerView.java
+++ app/src/main/java/com/example/gamestate/CheckerView.java
@@ -25,8 +25,11 @@ public class CheckerView extends SurfaceView implements View.OnTouchListener{
 
     public CheckerView(Context context) {
         super(context);
+        setWillNotDraw(false);
+
         pieces = new Pieces[8][8];
         board = new int[8][8];
+
         size = 100;
         left = top = 50;
         right = left + size;
@@ -36,23 +39,90 @@ public class CheckerView extends SurfaceView implements View.OnTouchListener{
         squareColor.setColor(Color.BLACK);
         highLight = new Paint();
         highLight.setColor(Color.GREEN);
+
+        placePieces();
+    }
+
+
+    public void placePieces() {
+        for (int row = 0; row < pieces.length; row++) {
+            for (int col = 0; col < pieces[row].length; col++) {
+
+                //fill first and third row with black pieces
+                if (col == 0 || col == 2) {
+                    if (row % 2 == 0) {
+                        pieces[row][col] = new Pieces(0, Pieces.Colors.BLACK, row, col);
+                    }
+
+                    //fill rest of first and third row with empty pieces
+                    else {
+                        pieces[row][col] = new Pieces(0, Pieces.Colors.EMPTY, row, col);
+                    }
+                }
+
+                //fill second row with black pieces
+                else if (col == 1) {
+                    if (row % 2 != 0) {
+                        pieces[row][col] = new Pieces(0, Pieces.Colors.BLACK, row, col);
+                    }
+                    //fill rest of second row with empty pieces
+                    else {
+                        pieces[row][col] = new Pieces(0, Pieces.Colors.EMPTY, row, col);
+                    }
+                }
+
+                //fill sixth and eighth row with red pieces
+                else if (col == 5 || col == 7) {
+                    if (row % 2 == 0) {
+                        pieces[row][col] = new Pieces(0, Pieces.Colors.RED, row, col);
+                    }
+                    //fill rest of sixth and eighth row with empty pieces
+                    else {
+                        pieces[row][col] = new Pieces(0, Pieces.Colors.EMPTY, row, col);
+                    }
+                }
+
+                //fill seventh row with red pieces
+                else if (col == 6) {
+                    if (row % 2 != 0) {
+                        pieces[row][col] = new Pieces(0, Pieces.Colors.RED, row, col);
+                    }
+                    //fill rest of seventh row with empty pieces
+                    else {
+                        pieces[row][col] = new Pieces(0, Pieces.Colors.EMPTY, row, col);
+                    }
+                }
+                //fill rest of board with empty pieces
+                else {
+                    pieces[row][col] = new Pieces(0, Pieces.Colors.EMPTY, row, col);
+                }
+            }
+        }
     }
 
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        //board initialization
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
+
+
+                //alternate colors of squares to create checkerboard pattern
                 if ((i % 2 == 0 && j % 2 != 0) || (j % 2 == 0 && i % 2 != 0)) {
                     squareColor.setColor(Color.WHITE);
                 } else {
                     squareColor.setColor(Color.BLACK);
                 }
-                canvas.drawRect(left+(right-left) * i, top+(bottom-top)*j,right+
-                        (right-left)*i, bottom+(bottom-top)*j,squareColor);
+
+                //draw rectangle
+                canvas.drawRect(left + (right - left) * i, top + (bottom - top) * j, right +
+                        (right - left) * i, bottom + (bottom - top) * j, squareColor);
             }
         }
     }
+
+
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
